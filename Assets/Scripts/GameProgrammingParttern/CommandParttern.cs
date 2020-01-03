bcd79670941d80062d014d5d3c203970c1db5785
yeonhong park <yeonhong84@gmail.com>
2020-01-04 01:15:58 +0900

    커맨드 패턴 원형.

---
+++ Assets/Scripts/GameProgrammingParttern/CommandParttern.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ProgrammingPattern
+{
+    public class Actor
+    {
+        public Vector3 position;
+    }
+
+    // CommandParttern
+    public class Command
+    {
+        public virtual void Excute(Actor actor) { }
+    }
+
+    public class JumpCommand : Command
+    {
+        public override void Excute(Actor actor)
+        {
+            actor.position.y--;
+        }
+    }
+}
