4ce1b5b3688d66fc57be0c19f4a96e134602e1b7
yeonhong park <yeonhong84@gmail.com>
2020-01-04 23:07:03 +0900

    옵저버 패턴 연습

---
+++ Assets/Scripts/GameProgrammingParttern/ObserverPartten.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace ProgrammingPattern
+{
+	// observer.
+
+	#region 전통적인 구조?
+	public class SampleEventArgs
+	{
+		public SampleEventArgs(string s) { Text = s; }
+		public String Text { get; } // readonly
+	}
+
+	public class Publisher
+	{
+		// Declare the delegate (if using non-generic pattern).
+		public delegate void SampleEventHandler(object sender, SampleEventArgs e);
+
+		// Declare the event.
+		public event SampleEventHandler SampleEvent;
+
+		// Wrap the event in a protected virtual method
+		// to enable derived classes to raise the event.
+		protected virtual void RaiseSampleEvent()
+		{
+			// Raise the event in a thread-safe manner using the ?. operator.
+			SampleEvent?.Invoke(this, new SampleEventArgs("Hello"));
+		}
+	}
+
+	public class ObserverTest
+	{
+		Publisher pub_ = new Publisher();
+
+		ObserverTest()
+		{
+			pub_.SampleEvent += ScbscribeMethod;
+		}
+
+		~ObserverTest()
+		{
+			pub_.SampleEvent -= ScbscribeMethod;
+		}
+
+		private void ScbscribeMethod(object sender, SampleEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	} 
+	#endregion
+
+	public class ObserverUnity
+	{
+		UnityEvent OnDummyEvent = new UnityEvent();
+
+		void Invoking()
+		{
+			OnDummyEvent.Invoke();
+		}
+
+		void Add()
+		{
+			OnDummyEvent.AddListener(OnListen);
+		}
+
+		private void OnListen()
+		{
+			throw new NotImplementedException();
+		}
+
+		void Remove()
+		{
+			OnDummyEvent.RemoveListener(OnListen);
+		}
+	}
+}
