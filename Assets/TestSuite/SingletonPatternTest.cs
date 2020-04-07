7d01273d1f26a2c30968abde4b7577942cc12241
yeonhong park <yeonhong84@gmail.com>
2020-04-07 19:55:53 +0900

    싱글턴 테스트

---
+++ Assets/TestSuite/SingletonPatternTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using ProgrammingPattern;
+using UnityEngine;
+
+namespace Tests.ProgrammingPattern
+{
+	public class SingleMono : Singleton<SingleMono>
+	{
+		public int SingleValue = 0;
+	}
+
+	public class SingletonPatternTest
+	{
+		[SetUp]
+		public void Setup() {
+		}
+
+		[Test]
+		public void MakeSingleton() {
+			SingleMono.Instance.SingleValue = 1;
+			Assert.IsTrue(SingleMono.Instance.SingleValue == 1);
+		}
+
+		[Test]
+		public void FindSingleton() {
+			SingleMono.Instance.SingleValue = 1;
+			var singletonObject = GameObject.Find("Tests.ProgrammingPattern.SingleMono");
+			Assert.IsNotNull(singletonObject);
+		}
+	}
+}
