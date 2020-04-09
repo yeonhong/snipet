e9f0e4ff04f3757d0a572be58b7f766cb99eb7a8
yeonhong park <yeonhong84@gmail.com>
2020-04-09 16:45:53 +0900

    테스트코드 한글로 명명

--- Assets/TestSuite/SingletonPatternTest.cs
+++ Assets/TestSuite/SingletonPatternTest.cs
@@ -13,19 +13,25 @@ namespace Tests.ProgrammingPattern
 	{
 		[SetUp]
 		public void Setup() {
+			SingleMono.Instance.SingleValue = 100;
 		}
 
 		[Test]
-		public void MakeSingleton() {
+		public void 싱글턴값_변경적용() {
 			SingleMono.Instance.SingleValue = 1;
 			Assert.IsTrue(SingleMono.Instance.SingleValue == 1);
 		}
 
 		[Test]
-		public void FindSingleton() {
-			SingleMono.Instance.SingleValue = 1;
+		public void 싱글턴오브젝트찾기() {
 			var singletonObject = GameObject.Find("Tests.ProgrammingPattern.SingleMono");
 			Assert.IsNotNull(singletonObject);
+		}
+
+		[Test]
+		public void 한개만생성되는지체크() {
+			var objects = GameObject.FindObjectsOfType<SingleMono>();
+			Assert.IsTrue(objects.Length == 1);
 		}
 	}
 }
