56a67d4cc282e8cb477166cbc55df6c9b9f8972a
yeonhong park <yeonhong84@gmail.com>
2020-05-08 17:25:43 +0900

    Tdd연습 - 3 https://youtu.be/hjcBgmMN8R4

---
+++ Assets/Tests/EditMode/TDD in Unity/HeartContainerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TDD_in_Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartContainerTests
+{
+	Image _image_last;
+	Image _image_first;
+	HeartContainer _heartContainer;
+
+	[SetUp]
+	public void BeforeTests() {
+		_image_last = new GameObject().AddComponent<Image>();
+		_image_last.fillAmount = 0f;
+		_image_first = new GameObject().AddComponent<Image>();
+		_image_first.fillAmount = 0f;
+
+		_heartContainer = new HeartContainer(new List<Heart> { new Heart(_image_first), new Heart(_image_last) });
+	}
+
+	public class Replenish함수 : HeartContainerTests
+	{
+		[Test]
+		public void _0_0에서_0으로_설정하기() {
+			_heartContainer.Replenish(0);
+			Assert.AreEqual(0, _image_last.fillAmount);
+		}
+
+		[Test]
+		public void _1_0에서_25퍼센트로_설정하기() {
+			_image_first.fillAmount = 1f;
+			_image_last.fillAmount = 0f;
+
+			_heartContainer.Replenish(1);
+			Assert.AreEqual(0.25f, _image_last.fillAmount);
+		}
+
+		[Test]
+		public void _1_완전_0에서_25퍼센트로_설정하기() {
+			_image_first.fillAmount = 0f;
+			_image_last.fillAmount = 0f;
+
+			_heartContainer.Replenish(1);
+			Assert.AreEqual(0.25f, _image_first.fillAmount);
+			Assert.AreEqual(0f, _image_last.fillAmount);
+		}
+	}
+}
+
+public class HeartContainer
+{
+	private List<Heart> _list;
+
+	public HeartContainer(List<Heart> list) {
+		this._list = list;
+	}
+
+	public void Replenish(int numberOfHeartPieces) {
+
+		foreach(var heart in _list) {
+			heart.Replenish(numberOfHeartPieces);
+		}
+	}
+}
