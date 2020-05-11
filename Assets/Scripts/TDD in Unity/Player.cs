8db369a3a10f24017c9ead33107e7479ed5ea13c
yeonhong park <yeonhong84@gmail.com>
2020-05-11 17:31:56 +0900

    TDD in Unity 7 플레이어 체력항목을 추가 https://youtu.be/_vd2JyX6C1Y

---
+++ Assets/Scripts/TDD in Unity/Player.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace TDD_in_Unity
+{
+	public class Player
+	{
+		public int CurrentHealth { get; private set; }
+		public int MaximumHealth { get; private set; }
+
+		public Player(int currentHealth, int maximumHealth = 12) {
+			if (currentHealth < 0) {
+				throw new ArgumentOutOfRangeException(nameof(currentHealth), "음수값은 안됨");
+			}
+
+			if (currentHealth > maximumHealth) {
+				throw new ArgumentOutOfRangeException(nameof(currentHealth), "최대값보다 현재값이 크다");
+			}
+
+			CurrentHealth = currentHealth;
+			MaximumHealth = maximumHealth;
+		}
+
+		public void Heal(int amount) {
+			CurrentHealth = Mathf.Min(CurrentHealth + amount, MaximumHealth);
+		}
+
+		public void Damage(int amount) {
+			CurrentHealth = Mathf.Max(CurrentHealth - amount, 0);
+		}
+	}
+}
---
+++ Assets/Tests/EditMode/TDD in Unity/PlayerTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TDD_in_Unity;
+
+public class PlayerTests
+{
+	public class CurrentHealth속성
+	{
+		[Test]
+		public void Health의_기본값은_0이다() {
+			var player = new Player(0);
+
+			Assert.AreEqual(0, player.CurrentHealth);
+		}
+
+		[Test]
+		public void Health의_초기값은_0이하의_값을_넣을수_없다() {
+			Assert.Throws<ArgumentOutOfRangeException>(() => {
+				new Player(-1);
+			});
+		}
+
+		[Test]
+		public void Health의_초기값은_최대값을_넘길수_없다() {
+			Assert.Throws<ArgumentOutOfRangeException>(() => {
+				new Player(2, 1);
+			});
+		}
+	}
+
+	public class Heal함수
+	{
+		[Test]
+		public void _0을_넣으면_아무일도_안생긴다() {
+			var player = new Player(0);
+
+			player.Heal(0);
+
+			Assert.AreEqual(0, player.CurrentHealth);
+		}
+
+		[Test]
+		public void 체력을_1회복시키기() {
+			var player = new Player(0);
+
+			player.Heal(1);
+
+			Assert.AreEqual(1, player.CurrentHealth);
+		}
+
+		[Test]
+		public void 최대체력이상으로_회복되지_않는다() {
+			var player = new Player(0);
+
+			player.Heal(100);
+
+			Assert.AreEqual(player.MaximumHealth, player.CurrentHealth);
+		}
+	}
+
+	public class Damage함수
+	{
+		[Test]
+		public void 데미지를_0을_받으면_아무일도_안생긴다() {
+			var player = new Player(1);
+
+			player.Damage(0);
+
+			Assert.AreEqual(1, player.CurrentHealth);
+		}
+
+		[Test]
+		public void 데미지를_1을_받으면_1이_깍인다() {
+			var player = new Player(1);
+
+			player.Damage(1);
+
+			Assert.AreEqual(0, player.CurrentHealth);
+		}
+
+		[Test]
+		public void 데미지를_크게받아도_0이하로_내려가지_않는다() {
+			var player = new Player(1);
+
+			player.Damage(100);
+
+			Assert.AreEqual(0, player.CurrentHealth);
+		}
+	}
+}
