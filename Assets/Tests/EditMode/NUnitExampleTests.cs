d2afab5a43ba7935db5d4e34554a3898117cc927
yeonhong park <yeonhong84@gmail.com>
2020-05-12 16:42:55 +0900

    테스트코드 명명법 정리

--- Assets/Tests/EditMode/NUnitExampleTests.cs
+++ Assets/Tests/EditMode/NUnitExampleTests.cs
@@ -4,6 +4,15 @@ using System;
 // namespace로 메뉴에서 계층구조로 분류가 가능하다
 namespace Editor
 {
+	/*
+	 * 테스트 명명법 정리
+	 * 상위 클래스틑 테스트할 대상 클래스이름 + Tests (ex. PlayerTests)
+	 * inner클래스의 경우는 대상 클래스의 함수나 프로퍼티를 테스트 (ex. CurrentHpPropertyTests)
+	 * 테스트의 경우 [시나리오]_[기대값] (ex. 체력이상으로회복할때_최대체력까지만회복된다)
+	 * 
+	 * 테스트코드의 순서 // arrage (정렬) // act (동작) // assert (확인)
+	 */
+
 	[TestFixture]
 	public class NUnitExampleTests
 	{
