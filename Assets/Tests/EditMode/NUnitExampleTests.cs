fe1375dd310986a0cb0eef0012811592b30a528a
yeonhong park <yeonhong84@gmail.com>
2020-05-13 17:07:56 +0900

    attribute test

--- Assets/Tests/EditMode/NUnitExampleTests.cs
+++ Assets/Tests/EditMode/NUnitExampleTests.cs
@@ -1,5 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections;
+using UnityEngine;
+using RangeAttribute = NUnit.Framework.RangeAttribute;
 
 // namespace로 메뉴에서 계층구조로 분류가 가능하다
 namespace NUnitExample
@@ -11,11 +14,20 @@ namespace NUnitExample
 	 * 테스트의 경우 [시나리오]_[기대값] (ex. 체력이상으로회복할때_최대체력까지만회복된다)
 	 * 
 	 * 테스트코드의 순서 // arrage (정렬) // act (동작) // assert (확인)
+	 * 
+	 * Attribute는 아래 링크에서.
+	 * https://github.com/nunit/docs/wiki/Attributes
 	 */
 
 	[TestFixture]
+	[TestFixtureSource(typeof(FixtureArgs))]
 	public class NUnitExampleTests
 	{
+		public NUnitExampleTests(string arg, int val) {
+			// 테스트 소스에 설정한 만큼 반복 테스트함.
+			// Debug.Log($"{arg} {val}");
+		}
+
 		[OneTimeSetUp]
 		public void BeforeTestsOnce() {
 			// 모든 테스트 시작 전에 한번만 실행.
@@ -57,6 +69,22 @@ namespace NUnitExample
 			});
 		}
 
+		[Test]
+		public void Values_Range매개변수설정([Values(1, 2, 3)] int x, [Range(0.2f, 0.6f, 0.1f)] float f) {
+			// Values에 넣은 값 갯수 만큼 반복 테스트.
+			// Range에 넣은 값 범위 만큼 반복 테스트
+			// Debug.Log($"{x} {f}");
+			Assert.IsTrue(true);
+		}
+
+		[TestCase(12, 3, 4)]
+		[TestCase(12, 2, 6)]
+		[TestCase(12, 4, 3)]
+		public void DivideTest(int n, int d, int q) {
+			// 테스트 케이스만큼 반복.
+			Assert.AreEqual(q, n / d);
+		}
+
 		[TestFixture]
 		public class InnerTests
 		{
@@ -68,4 +96,14 @@ namespace NUnitExample
 			}
 		}
 	}
+
+	// https://github.com/nunit/docs/wiki/TestFixtureSource-Attribute 참고.
+	class FixtureArgs : IEnumerable
+	{
+		public IEnumerator GetEnumerator() {
+			yield return new object[] { "Arg1", 1 };
+			yield return new object[] { "Arg2", 2 };
+		}
+	}
+
 }
