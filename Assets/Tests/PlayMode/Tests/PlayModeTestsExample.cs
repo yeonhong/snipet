bbfbea44acf37649ae84a8ef57d3e0e6e83c0cb3
yeonhong park <yeonhong84@gmail.com>
2020-05-15 16:36:04 +0900

    플레이모드 기본 테스트

---
+++ Assets/Tests/PlayMode/Tests/PlayModeTestsExample.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	[TestFixture]
+	public class PlayModeTestsExample
+	{
+		[OneTimeSetUp]
+		public void LoadTestScene() {
+			SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
+		}
+
+		[UnityTest]
+		public IEnumerator 카메라컴포넌트는_한개이상이어야한다() {
+			var cameras = Object.FindObjectsOfType<Camera>();
+			Assert.IsTrue(cameras.Length > 0);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator MainCamera오브젝트가_있다() {
+			var cam = GameObject.Find("Main Camera");
+			Assert.IsNotNull(cam);
+			yield return null;
+		}
+
+		[UnityTest]
+		[UnityPlatform(RuntimePlatform.WindowsEditor)]
+		//[UnityPlatform(exclude = new[] { RuntimePlatform.WindowsEditor })]
+		public IEnumerator GameObject_WithRigidBody_WillBeAffectedByPhysics() {
+			var go = new GameObject();
+			go.AddComponent<Rigidbody>();
+			var originalPosition = go.transform.position.y;
+
+			yield return new WaitForFixedUpdate();
+
+			Assert.AreNotEqual(originalPosition, go.transform.position.y);
+		}
+
+		[UnityTest]
+		public IEnumerator MonoBehaviourTest_Works() {
+			yield return new MonoBehaviourTest<MyMonoBehaviourTest>();
+		}
+
+		public class MyMonoBehaviourTest : MonoBehaviour, IMonoBehaviourTest
+		{
+			private int frameCount;
+			public bool IsTestFinished {
+				get { return frameCount > 10; }
+			}
+
+			void Update() {
+				frameCount++;
+			}
+		}
+	}
+}
