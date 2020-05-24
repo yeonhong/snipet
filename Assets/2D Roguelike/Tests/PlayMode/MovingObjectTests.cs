5e6002e5b1fc0adddcb00e3864931e573d223ece
yeonhong park <yeonhong84@gmail.com>
2020-05-24 23:51:21 +0900

    add Movingobject test

---
+++ Assets/2D Roguelike/Tests/PlayMode/MovingObjectTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	[TestFixture]
+    public class MovingObjectTests
+    {
+        [UnityTest]
+        public IEnumerator MovingObjectTestsWithEnumeratorPasses()
+        {
+            yield return null;
+        }
+    }
+}
