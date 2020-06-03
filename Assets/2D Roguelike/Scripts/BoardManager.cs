05296f50fc565d581fb2d1287cfff14dc1cbdb52
yeonhong park <yeonhong84@gmail.com>
2020-06-03 17:08:31 +0900

    refactor : boardmanager

--- Assets/2D Roguelike/Scripts/BoardManager.cs
+++ Assets/2D Roguelike/Scripts/BoardManager.cs
@@ -21,6 +21,11 @@ namespace Roguelike2D
 				minimum = min;
 				maximum = max;
 			}
+
+			public Count(int value) {
+				minimum = value;
+				maximum = value;
+			}
 		}
 
 		[SerializeField] private int columns = 8;
@@ -34,81 +39,127 @@ namespace Roguelike2D
 		[SerializeField] private GameObject[] enemyTiles = null;
 		[SerializeField] private GameObject[] outerWallTiles = null;
 
-		private Transform boardHolder;
-		private List<Vector3> gridPositions = new List<Vector3>();
 		public CreateEnemyEvent OnEnemyCreated = new CreateEnemyEvent();
 
-		private void InitialiseList() {
-			gridPositions.Clear();
+		private List<Vector3> _gridPositions = new List<Vector3>();
 
-			for (int x = 1; x < columns - 1; x++) {
-				for (int y = 1; y < rows - 1; y++) {
-					gridPositions.Add(new Vector3(x, y, 0f));
-				}
-			}
+		public void SetupScene(int level) {
+			SetUpBoard();
+			InitGridPositions();
+
+			Count enemyCount = new Count((int)Mathf.Log(level, 2f));
+
+			LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum, null);
+			LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum, null);
+			LayoutObjectAtRandom(enemyTiles, enemyCount.minimum, enemyCount.maximum, OnEnemyCreated);
+			LayoutExitObject();
 		}
 
-		private void BoardSetup() {
-			boardHolder = new GameObject("Board").transform;
+		private void LayoutExitObject() {
+			Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+		}
+
+		private void SetUpBoard() {
+			var boardHolder = new GameObject("Board").transform;
 
 			for (int x = -1; x < columns + 1; x++) {
 				for (int y = -1; y < rows + 1; y++) {
-					GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
 
-					if (x == -1 || x == columns || y == -1 || y == rows) {
-						toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
-					}
-
-					var instance =
-						Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+					var instance = Instantiate(
+						GetBoardObject(x, y),
+						new Vector3(x, y, 0f),
+						Quaternion.identity
+					);
 
 					instance.transform.SetParent(boardHolder);
 				}
 			}
 		}
 
-		private Vector3 RandomPosition() {
-			int randomIndex = Random.Range(0, gridPositions.Count);
-			Vector3 randomPosition = gridPositions[randomIndex];
-			gridPositions.RemoveAt(randomIndex);
-			return randomPosition;
+		private GameObject GetBoardObject(int x, int y) {
+			return IsBorderLine(x, y) ? GetRandomOuterWall() : GetRandomFloor();
 		}
 
-		private void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum, UnityEvent<GameObject> OnCreated = null) {
-			int objectCount = Random.Range(minimum, maximum + 1);
+		private GameObject GetRandomFloor() {
+			return floorTiles[Random.Range(0, floorTiles.Length)];
+		}
 
-			for (int i = 0; i < objectCount; i++) {
-				var randomPosition = RandomPosition();
-				var tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-				var instance = Instantiate(tileChoice, randomPosition, Quaternion.identity);
-				OnCreated?.Invoke(instance);
+		private GameObject GetRandomOuterWall() {
+			return outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+		}
+
+		private bool IsBorderLine(int x, int y) {
+			return x == -1 || x == columns || y == -1 || y == rows;
+		}
+
+		private void InitGridPositions() {
+			_gridPositions.Clear();
+
+			for (int xPos = 1; xPos < columns - 1; xPos++) {
+				for (int yPos = 1; yPos < rows - 1; yPos++) {
+					_gridPositions.Add(new Vector3(xPos, yPos, 0f));
+				}
 			}
 		}
 
-		public void SetupScene(int level) {
-			BoardSetup();
-			InitialiseList();
-			LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
-			LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
-			int enemyCount = (int)Mathf.Log(level, 2f);
-			LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount, OnEnemyCreated);
-			Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+		private void LayoutObjectAtRandom(GameObject[] objects, 
+			int min, int max, UnityEvent<GameObject> OnCreated = null) {
+
+			int cntCreate = Random.Range(min, max + 1);
+
+			for (int i = 0; i < cntCreate; i++) {
+				var newObject = Instantiate(
+					GetRandomObject(objects),
+					GetRandomEmptyPosition(),
+					Quaternion.identity
+				);
+
+				OnCreated?.Invoke(newObject);
+			}
+		}
+
+		private static GameObject GetRandomObject(GameObject[] objects) {
+			return objects[Random.Range(0, objects.Length)];
+		}
+
+		private Vector3 GetRandomEmptyPosition() {
+			var randomIndex = Random.Range(0, _gridPositions.Count);
+			var result = _gridPositions[randomIndex];
+			_gridPositions.RemoveAt(randomIndex);
+			return result;
 		}
 
 		#region Validation Tests
 #if UNITY_EDITOR
 		public bool ValidateResources() {
 
-			if (exit == null) return false;
-			if (floorTiles == null) return false;
-			if (wallTiles == null) return false;
-			if (foodTiles == null) return false;
-			if (enemyTiles == null) return false;
-			if (outerWallTiles == null) return false;
+			if (exit == null) {
+				return false;
+			}
+
+			if (floorTiles == null) {
+				return false;
+			}
+
+			if (wallTiles == null) {
+				return false;
+			}
+
+			if (foodTiles == null) {
+				return false;
+			}
+
+			if (enemyTiles == null) {
+				return false;
+			}
+
+			if (outerWallTiles == null) {
+				return false;
+			}
 
 			return true;
 		}
-#endif 
+#endif
 		#endregion
 	}
 }
