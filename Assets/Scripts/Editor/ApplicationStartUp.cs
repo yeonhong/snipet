36f52fca44313022bbb4838e7417359a779b9ff3
yeonhong park <yeonhong84@gmail.com>
2020-06-08 13:19:56 +0900

    유니티가 켜질때 한번만 실행하는 코드

---
+++ Assets/Scripts/Editor/ApplicationStartUp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Project.Editor
+{
+	[InitializeOnLoad]
+	public class ApplicationStartUp : ScriptableObject
+	{
+		static ApplicationStartUp _instance = null;
+
+		static ApplicationStartUp() {
+			EditorApplication.update += OnInit;
+		}
+
+		static void OnInit() {
+			EditorApplication.update -= OnInit;
+
+			_instance = FindObjectOfType<ApplicationStartUp>();
+			if (_instance == null) {
+				_instance = CreateInstance<ApplicationStartUp>();
+
+				OnApplicationLoaded();
+			}
+		}
+
+		static void OnApplicationLoaded() {
+			Debug.Log("OnApplicationLoaded");
+		}
+	}
+}
